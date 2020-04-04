331dd629421ab8d9004db6d0c7207e0a7eaedb5e
cgarcialm <cgarcialm@github.com>
2020-04-04 11:56:40 -0300

    _first readme

---
+++ htmls/readme.py
@@ -0,0 +1,17 @@
+# Foobar
+
+This is a preview of the README file.
+
+## Installation
+
+For usage you will need Anylogic
+
+## Usage
+
+Download input files and PowerBI
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
